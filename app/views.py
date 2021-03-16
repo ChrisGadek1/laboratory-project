92325592d2d355830fe0c796123181274e683e21
ChrisGadek1 <krzysztof.gadek15@gmail.com>
2021-03-16 22:24:43 +0100

    first main_site commit

--- app/views.py
+++ app/views.py
@@ -1,14 +1,8 @@
-from django.contrib.auth import get_user_model
 from django.shortcuts import render
-from django.shortcuts import redirect
-from .forms import RawLoginForm
 
 # Create your views here.
 
 
-def main_view(request):
-    return render(request, 'basic.html')
+def main_site(request, *args, **kwargs):
+    return render(request, 'main_logged.html', {})
 
-
-def main_site(request):
-    return render(request, 'main.html')
