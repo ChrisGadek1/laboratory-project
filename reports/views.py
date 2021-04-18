8a0df80c8437c6a3daf556c4b59c5034b9b8e9ee
ChrisGadek1 <krzysztof.gadek15@gmail.com>
2021-04-19 01:58:17 +0200

    choosing templates

--- reports/views.py
+++ reports/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from samples.forms import SampleForm, ResearchForm
-
+import os
+from samples.models import Sampling
 # Create your views here.
 
 
@@ -15,18 +16,22 @@ def choose_mode(request, *args, **kwargs):
 def add_template(request, *args, **kwargs):
     context = {}
     if request.user.is_authenticated:
-        if request.method == "POST" and request.FILES['myfile']:
+        fs = FileSystemStorage()
+        if request.method == "POST" and request.POST.get("mode", "") == "add" and request.FILES['myfile']:
             myfile = request.FILES['myfile']
-            fs = FileSystemStorage()
             filename = fs.save(myfile.name, myfile)
             uploaded_file_url = fs.url(filename)
-        else:
-            samples_labels = {v: k for k, v in SampleForm.Meta.labels.items()}
-            research_labels = {v: k for k, v in ResearchForm.Meta.labels.items()}
-            context = {
-                "samples_fields": samples_labels,
-                "research_fields": research_labels
-            }
+        elif request.method == "POST"and request.POST.get("mode", "") == "delete":
+            name = request.POST.get("choose-to-delete", "")
+            fs.delete(name)
+
+        samples_labels = {v: k for k, v in SampleForm.Meta.labels.items()}
+        research_labels = {v: k for k, v in ResearchForm.Meta.labels.items()}
+        context = {
+            "samples_fields": samples_labels,
+            "research_fields": research_labels,
+            "reports": os.listdir("media/")
+        }
         return render(request, 'reports/add_template.html', context)
     else:
         return render(request, 'main_not_logged.html', {})
@@ -34,6 +39,9 @@ def add_template(request, *args, **kwargs):
 
 def generate_report(request, *args, **kwargs):
     if request.user.is_authenticated:
-        return render(request, 'reports/reports.html', {})
+        context = {}
+        context["samples"] = Sampling.objects.all()
+        context["reports"] = os.listdir("media/")
+        return render(request, 'reports/reports.html', context)
     else:
         return render(request, 'main_not_logged.html', {})
